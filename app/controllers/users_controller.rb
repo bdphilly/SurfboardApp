9bf867683e6b7d5cb81570e7c5e6e10036a5c0fb
Bryan Phillips <bdphilly@gmail.com>
2014-05-20 22:41:37 -0700

    users controller, sessions controller, application controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+	before_action :require_signed_out!, only: [:new, :create]
+	before_action :require_signed_in!, only: [:show]
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+	
+		if @user.save
+			sign_in(@user)
+			redirect_to user_url(@user)
+		else
+			flash.now[:errors] = @user.errors.full_messages
+			render :new
+		end
+	end
+
+	def show
+			@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update_attributes(user_params)
+			redirect_to user_url(@user)
+		else
+			flash.now[:errors] = @user.errors.full_messages
+			render :edit
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to new_session_url
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :password)
+	end
+end
