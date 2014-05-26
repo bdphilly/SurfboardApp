3c4b1f36529a2ad0978ed790b94952cf013ff05b
Bryan Phillips <bdphilly@gmail.com>
2014-05-26 11:38:41 -0700

    mini calendar works. css: cal2

--- app/assets/javascripts/views/boards/calendar_page.js
+++ app/assets/javascripts/views/boards/calendar_page.js
@@ -2,46 +2,34 @@ SurfboardApp.Views.CalendarPage = Backbone.View.extend({
 
   template: JST['boards/calendar'],
 
-  initialize: function () {
-  },
-
   render: function () {
     var renderedContent = this.template();
+
     this.$el.html(renderedContent); 
-    // this.$el.find('#mini-calendar').clndr({
-    //   template: $('#calendar-template').html()
-    // });
-    renderCalendar();
+    this.$el.find('.cal2').clndr({
+      template: this.calendarTemplate
+    });
+    // this.renderCalendar();
     return this;
   },
 
-  renderCalendar: function () {
-    var currentMonth = moment().format('YYYY-MM');
-    var nextMonth    = moment().add('month', 1).format('YYYY-MM');
-    var events = [
-      { date: currentMonth + '-' + '10', title: 'Persian Kitten Auction', location: 'Center for Beautiful Cats' },
-      { date: currentMonth + '-' + '19', title: 'Cat Frisbee', location: 'Jefferson Park' },
-      { date: currentMonth + '-' + '23', title: 'Kitten Demonstration', location: 'Center for Beautiful Cats' },
-      { date: nextMonth + '-' + '07',    title: 'Small Cat Photo Session', location: 'Center for Cat Photography' }
-    ];  
+  calendarTemplate: '<div id="calendar-template"><div class="clndr-controls"><div class="clndr-previous-button">&lsaquo;</div><div class="month"><%= month %></div><div class="clndr-next-button">&rsaquo;</div></div><div class="clndr-grid"><div class="days-of-the-week"><% _.each(daysOfTheWeek, function(day) { %><div class="header-day"><%= day %></div><% }); %><div class="days"><% _.each(days, function(day) { %><div class="<%= day.classes %>"><%= day.day %></div><% }); %></div></div></div><div class="clndr-today-button">Today</div></div>',
 
-    this.el$.find('#mini-calendar').clndr({
-      template: this.el$.find('#calendar-template').html(),
-      events: events,
-      clickEvents: {
-        click: function(target) {
-          if(target.events.length) {
-            var daysContainer = $('#mini-clndr').find('.days-container');
-            daysContainer.toggleClass('show-events', true);
-            $('#mini-clndr').find('.x-button').click( function() {
-              daysContainer.toggleClass('show-events', false);
-            });
-          }
-        }
-      },
-      adjacentDaysChangeMonth: true
-    });
-  },
+  // renderCalendar: function () {
+  //   var currentMonth = moment().format('YYYY-MM');
+  //   var nextMonth    = moment().add('month', 1).format('YYYY-MM');
+  //   var events = [
+  //     { date: currentMonth + '-' + '10', title: 'Persian Kitten Auction', location: 'Center for Beautiful Cats' },
+  //     { date: currentMonth + '-' + '19', title: 'Cat Frisbee', location: 'Jefferson Park' },
+  //     { date: currentMonth + '-' + '23', title: 'Kitten Demonstration', location: 'Center for Beautiful Cats' },
+  //     { date: nextMonth + '-' + '07',    title: 'Small Cat Photo Session', location: 'Center for Cat Photography' }
+  //   ];  
+
+  //   this.el$.find('#mini-calendar').clndr({
+  //     template: this.el$.find('#calendar-template').html(),
+  //     events: events,
+  //   });
+  // },
 
 
 
