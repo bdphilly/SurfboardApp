cff62ef53a1a9c28b170d69d0404487319d35674
Bryan Phillips <bdphilly@gmail.com>
2014-05-26 14:03:40 -0700

    add click handlers to calendar. display availability and cost

--- app/assets/javascripts/views/boards/calendar_page.js
+++ app/assets/javascripts/views/boards/calendar_page.js
@@ -13,17 +13,84 @@ SurfboardApp.Views.CalendarPage = Backbone.View.extend({
 
   
   renderCalendar: function () {
-    var calendarTemplate = '<div id="calendar-template"><div class="clndr-controls"><div class="clndr-previous-button">&lsaquo;</div><div class="month"><%= month %></div><div class="clndr-next-button">&rsaquo;</div></div><div class="clndr-grid"><div class="days-of-the-week"><% _.each(daysOfTheWeek, function(day) { %><div class="header-day"><%= day %></div><% }); %><div class="days"><% _.each(days, function(day) { %><div class="<%= day.classes %>"><%= day.day %></div><% }); %></div></div></div><div class="clndr-today-button">Today</div></div>';
+    var calendarTemplate = '<div id="calendar-template"><div class="clndr-controls"><div class="clndr-previous-button">&lsaquo;</div><div class="month"><%= month %></div><div class="clndr-next-button">&rsaquo;</div></div><div class="clndr-grid"><div class="days-of-the-week"><% _.each(daysOfTheWeek, function(day) { %><div class="header-day"><%= day %></div><% }); %><div class="days"><% _.each(days, function(day) { %><div class="<%= day.classes %>"><%= day.day %><br><% if(day.events[0]) { %><%= day.events[0].cost %><br><%= day.events[0].status %><% } %></div><% }); %></div></div></div><div class="clndr-today-button">Today</div></div>';
+    // var templateCalendar = '<div id="template-calendar"><div class="clndr-controls"><div class="clndr-previous-button">&lsaquo;</div><div class="month"><%= month %></div><div class="clndr-next-button">&rsaquo;</div></div><div class="clndr-grid"><div class="days-of-the-week"><% _.each(daysOfTheWeek, function(day) { %><div class="header-day"><%= day %></div><% }); %><div class="days"><% _.each(days, function(day) { %><div class="<%= day.classes %>"><%= day.day %></div><% }); %></div></div><div class="event-listing"><div class="event-listing-title">EVENTS THIS MONTH</div><% _.each(eventsThisMonth, function(event) { %><div class="event-item"><div class="event-item-name"><%= event.name %></div><div class="event-item-location"><%= event.location %></div></div><% }); %></div></div><div class="clndr-today-button">Today</div></div>'
 
-    this.$el.find('.cal1').clndr({
-      template: calendarTemplate
-    });
+    // <% if(day.events[0].status) { %>
+    //     <%= day.events[0].cost %>
+    //   <% } %>
+
+    var currentMonth = moment().format('YYYY-MM');
+    var nextMonth    = moment().add('month', 1).format('YYYY-MM');
+    
+    var events = [
+      { date: currentMonth + '-' + '10', status: 'Available', cost: '$15' },
+      { date: currentMonth + '-' + '19', status: 'Unavailable', cost: '$10' },
+      { date: currentMonth + '-' + '23', status: 'Available', cost: '$20' },
+      { date: nextMonth + '-' + '07',    title: 'Small Cat Photo Session', location: 'Center for Cat Photography' }
+    ];
+
+    // this.$el.find('.cal1').clndr({
+    //   template: calendarTemplate
+    // });
 
     this.$el.find('.cal2').clndr({
-      template: calendarTemplate
+      template: calendarTemplate,
+      events: events,
+
+      
+
+      clickEvents: {
+        click:         function(target) { 
+          console.log('click');
+          console.log(target.events[0].status);
+          console.log(target.events[0].cost);
+           
+        },
+        nextMonth:     function(month)  { console.log('next month'); },
+        previousMonth: function(month)  { console.log('previous month'); },
+        nextYear:      function(month)  { console.log('next year'); },
+        previousYear:  function(month)  { console.log('previous year'); },
+        today:         function(month)  { console.log('today'); },
+        onMonthChange: function(month)  { console.log('on month change'); },
+        onYearChange:  function(month)  { console.log('on year change'); }
+      },
+
     });
   },
 
 
 
 });
+
+// <div id="template-calendar">
+//   <div class="clndr-controls">
+//     <div class="clndr-previous-button">&lsaquo;</div>
+//     <div class="month"><%= month %></div>
+//     <div class="clndr-next-button">&rsaquo;</div>
+//   </div>
+//   <div class="clndr-grid">
+//     <div class="days-of-the-week">
+//       <% _.each(daysOfTheWeek, function(day) { %>
+//         <div class="header-day"><%= day %></div>
+//       <% }); %>
+//       <div class="days">
+//         <% _.each(days, function(day) { %>
+//           <div class="<%= day.classes %>"><%= day.day %></div>
+//         <% }); %>
+//       </div>
+//     </div>
+
+//     <div class="event-listing">
+//       <div class="event-listing-title">EVENTS THIS MONTH</div>
+//       <% _.each(eventsThisMonth, function(event) { %>
+//           <div class="event-item">
+//             <div class="event-item-name"><%= event.name %></div>
+//             <div class="event-item-location"><%= event.location %></div>
+//           </div>
+//         <% }); %>
+//     </div>
+
+//   </div>
+//   <div class="clndr-today-button">Today</div>
+// </div>
