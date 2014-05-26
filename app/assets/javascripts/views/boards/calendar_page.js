864548cbd08be27766081cce0b78c1a0bd59731e
Bryan Phillips <bdphilly@gmail.com>
2014-05-26 15:39:56 -0700

    different color calendar cells for available and unavailable dates

--- app/assets/javascripts/views/boards/calendar_page.js
+++ app/assets/javascripts/views/boards/calendar_page.js
@@ -1,6 +1,7 @@
 SurfboardApp.Views.CalendarPage = Backbone.View.extend({
 
   template: JST['boards/calendar'],
+  template2: JST['boards/calendar2'],
 
   render: function () {
     var renderedContent = this.template();
@@ -13,12 +14,35 @@ SurfboardApp.Views.CalendarPage = Backbone.View.extend({
 
   
   renderCalendar: function () {
-    var calendarTemplate = '<div id="calendar-template"><div class="clndr-controls"><div class="clndr-previous-button">&lsaquo;</div><div class="month"><%= month %></div><div class="clndr-next-button">&rsaquo;</div></div><div class="clndr-grid"><div class="days-of-the-week"><% _.each(daysOfTheWeek, function(day) { %><div class="header-day"><%= day %></div><% }); %><div class="days"><% _.each(days, function(day) { %><div class="<%= day.classes %>"><%= day.day %><br><% if(day.events[0]) { %><%= day.events[0].cost %><br><%= day.events[0].status %><% } %></div><% }); %></div></div></div><div class="clndr-today-button">Today</div></div>';
-    // var templateCalendar = '<div id="template-calendar"><div class="clndr-controls"><div class="clndr-previous-button">&lsaquo;</div><div class="month"><%= month %></div><div class="clndr-next-button">&rsaquo;</div></div><div class="clndr-grid"><div class="days-of-the-week"><% _.each(daysOfTheWeek, function(day) { %><div class="header-day"><%= day %></div><% }); %><div class="days"><% _.each(days, function(day) { %><div class="<%= day.classes %>"><%= day.day %></div><% }); %></div></div><div class="event-listing"><div class="event-listing-title">EVENTS THIS MONTH</div><% _.each(eventsThisMonth, function(event) { %><div class="event-item"><div class="event-item-name"><%= event.name %></div><div class="event-item-location"><%= event.location %></div></div><% }); %></div></div><div class="clndr-today-button">Today</div></div>'
+   // var calendarTemplate = '<div id="calendar-template"><div class="clndr-controls"><div class="clndr-previous-button">&lsaquo;</div><div class="month"><%= month %></div><div class="clndr-next-button">&rsaquo;</div></div><div class="clndr-grid"><div class="days-of-the-week"><% _.each(daysOfTheWeek, function(day) { %><div class="header-day"><%= day %></div><% }); %><div class="days"><% _.each(days, function(day) { %><div class="<%= day.classes %>"><% if (day.events[0]) { %><% $(day).addClass("available") %>"><% } %><%= day.day %><br><% if(day.events[0]) { %><%= day.events[0].cost %><br><%= day.events[0].status %><% } %></div><% }); %></div></div></div><div class="clndr-today-button">Today</div></div>';
+
+   
+
+
+
+   // var calendarTemplate = '<div id="calendar-template"><div class="clndr-controls"><div class="clndr-previous-button">&lsaquo;</div><div class="month"><%= month %></div><div class="clndr-next-button">&rsaquo;</div></div><div class="clndr-grid"><div class="days-of-the-week"><% _.each(daysOfTheWeek, function(day) { %><div class="header-day"><%= day %></div><% }); %><div class="days"><% _.each(days, function(day) { %><%= day.day %><br><% if (day.events[0]) { %><%= day.events[0].cost %><br><%= day.events[0].status %><% } %></div><% }); %></div></div></div><div class="clndr-today-button">Today</div></div>';
+    
+
+// <% if (day.events[0].status === "Available") { %>
+//           <div class="<%= day.event-available %>">
+//         <% else { %> 
+//           <div class="<%= day.event-unavailable %>">
+//         <% } %>
+
+
+
+    // // var templateCalendar = '<div id="template-calendar"><div class="clndr-controls"><div class="clndr-previous-button">&lsaquo;</div><div class="month"><%= month %></div><div class="clndr-next-button">&rsaquo;</div></div><div class="clndr-grid"><div class="days-of-the-week"><% _.each(daysOfTheWeek, function(day) { %><div class="header-day"><%= day %></div><% }); %><div class="days"><% _.each(days, function(day) { %><div class="<%= day.classes %>"><%= day.day %></div><% }); %></div></div><div class="event-listing"><div class="event-listing-title">EVENTS THIS MONTH</div><% _.each(eventsThisMonth, function(event) { %><div class="event-item"><div class="event-item-name"><%= event.name %></div><div class="event-item-location"><%= event.location %></div></div><% }); %></div></div><div class="clndr-today-button">Today</div></div>'
 
     // <% if(day.events[0].status) { %>
     //     <%= day.events[0].cost %>
     //   <% } %>
+
+      // <% if (day.events[0].status) { %>
+      //   <% if ()}
+
+      // <div class="<%= day.classes %>">
+
+
 
     var currentMonth = moment().format('YYYY-MM');
     var nextMonth    = moment().add('month', 1).format('YYYY-MM');
@@ -34,8 +58,16 @@ SurfboardApp.Views.CalendarPage = Backbone.View.extend({
     //   template: calendarTemplate
     // });
 
+    var that = this;
+
     this.$el.find('.cal2').clndr({
-      template: calendarTemplate,
+      
+      render: function (data) {
+        return that.template2(data);
+      },
+
+
+      // template: this.template2(),
       events: events,
 
       
